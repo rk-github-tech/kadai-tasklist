e447e4b51f92bb65d5b8d5d3728f72ac2beb44b4
rk <culture.ir.123@gmail.com>
2021-02-02 10:50:41 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -23,11 +24,10 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
+
     if @task.save
       flash[:success] = '正常に更新されました'
       redirect_to task_url(@task)
@@ -38,14 +38,17 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-    
+  
     flash[:success] = '正常に削除されました'
     redirect_to tasks_url
   end
   
   private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
   
   def task_params
     params.require(:task).permit(:content)
